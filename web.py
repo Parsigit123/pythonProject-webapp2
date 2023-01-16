c5052c48943293abb0249fd85a5202d0397313d7
parsi <parsova.khayatan@gmail.com>
2023-01-16 10:30:59 +0100

    Initial commit.

---
+++ web.py
@@ -0,0 +1,33 @@
+import streamlit as st
+import functions
+
+
+todos = functions.get_todos()
+
+def add_todo():
+    todo = st.session_state["new_todo"] + "\n"
+#    print(todo)
+    todos.append(todo)
+    functions.write_todos(todos)
+
+
+st.title("My Todo App")
+st.subheader("This is my todo app.")
+st.write("This app is to increase your productivity.")
+
+#st.checkbox("Buy grocery1.")
+
+for index, todo in enumerate(todos):
+    checkbox = st.checkbox(todo, key=todo)
+    if checkbox:
+#        print(checkbox)
+        todos.pop(index)
+        functions.write_todos(todos)
+        del st.session_state[todo]
+        st.experimental_rerun()
+
+st.text_input(label="", placceholder=("Enter a todo:"),
+              on_change=add_todo, key='new_todo')
+
+#print("Hello")
+#t.session_state
